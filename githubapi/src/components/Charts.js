21c2f12d7884971eddac3878e75244f125cadb59
roisinburke <burker9@tcd.ie>
2020-12-29 16:10:43 +0000

    Added Chart and PieChart

--- githubapi/src/components/Charts.js
+++ githubapi/src/components/Charts.js
@@ -3,5 +3,21 @@ import {Bar} from 'react-chartjs-2';
 
 
 export default class Chart extends Component{
-   
+    constructor(props){
+        super(props);
+        this.state = {
+            chartData:props.chartData
+            
+        }
+    }
+    render(){
+        return(
+            <div className='chart'>
+                <Bar
+                data = {this.state.chartData} 
+                options = {{}}
+                />
+            </div>
+        )
+    }
 }
--- githubapi/src/components/PieChart.js
+++ githubapi/src/components/PieChart.js
@@ -3,5 +3,20 @@ import {Pie} from 'react-chartjs-2';
 
 
 export default class PieChart extends Component{
-    
+    constructor(props){
+        super(props);
+        this.state = {
+            pieChartData:props.pieChartData
+            
+        }
+    }
+    render(){
+        return(
+            <div className='chart'>
+                <Pie
+                data = {this.state.pieChartData} 
+                />
+            </div>
+        )
+    }
 }
